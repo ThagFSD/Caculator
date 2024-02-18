edcb2d757f75ff6b2d3da5cc907e9a9e1647cd6c
root <root@ThangFSD.localdomain>
2024-02-18 21:43:56 +0700

    fix script

--- script.js
+++ script.js
@@ -7,22 +7,19 @@ const keyDelete = document.querySelector(".key--delete");
 const keyClear = document.querySelector(".key--clear");
 const keyClearEntry = document.querySelector(".key--clearE");
 
-function firstDisplay() {
-    // if (currentDisplay.value === operator.textContent){
-    //     previousDisplay.append(currentDisplay.value);
-    // }
-}
 
 function secondDisplay() {
     currentDisplay.value = "0";
 }
 
 function clear() {
-
+    currentDisplay.value = "0";
+    previousDisplay.textContent = "";
+    keyOperators.value = undefined;
 }
 
 function clearEntry() {
-
+    currentDisplay.value = 0;
 }
 
 function append() {
@@ -34,22 +31,25 @@ function caculator() {
 }
 
 keyNumbers.forEach(number => {
-    number.addEventListener("click", function() {
+    number.addEventListener("click", function() { 
         if (currentDisplay.value === "0") {
-            currentDisplay.value = number.textContent;
-        }
-        else {
-            currentDisplay.value += number.textContent;
-        }
-        console.log("Numbers clicked !");
+            if (number.textContent === '.') {
+                currentDisplay.value += number.textContent;
+            } else {
+                currentDisplay.value = number.textContent;
+            }
+        } else {
+            // Check if the current display value already contains a decimal point
+            if (!(number.textContent === '.' && currentDisplay.value.includes('.'))) {
+                currentDisplay.value += number.textContent;
+            }
+        }   
     });
 });
 
 keyOperators.forEach(operator => {
     operator.addEventListener("click", function() {
         previousDisplay.textContent = currentDisplay.value + operator.textContent;
-        // currentDisplay.value = number.textContent;
-        // console.log("Operation clicked !");
     });
 });
 
@@ -62,10 +62,12 @@ keyDelete.addEventListener("click", function() {
 });
 
 keyClear.addEventListener("click", function() {
+    clear();
     // console.log("Clear clicked !");
 });
 
 keyClearEntry.addEventListener("click", function() {
+    clearEntry();
     // console.log("Clear entry clicked !");
 });
 
